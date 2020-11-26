0a22c0430c97354c442638982516a037f064618d
YasharMoradi <yasharm1377@gmail.com>
2020-11-26 19:01:59 +0330

    Updates

---
+++ precache-manifest.0d43111693ab83eb97bd3c653c8b54c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1c53d87f55325fbfadd28bb4e7319b",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "5f56d828451eadf9e26a",
+    "url": "/resume/static/css/main.9750aafc.chunk.css"
+  },
+  {
+    "revision": "e1fd977eeabd1df57d23",
+    "url": "/resume/static/js/2.2807b67b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/resume/static/js/2.2807b67b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f56d828451eadf9e26a",
+    "url": "/resume/static/js/main.cd25ffb9.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "2564515ceec99947c54009b56a923bc0",
+    "url": "/resume/static/media/logo.2564515c.jpg"
+  }
+]);
